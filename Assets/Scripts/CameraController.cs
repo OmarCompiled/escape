6886cdd1765d58b636ef71e6a8c17b1f15b72de0
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2026-05-02 17:16:29 +0300

    Tweak controls, texture terrain.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,20 +7,26 @@ public class CameraController : MonoBehaviour
     private float m_Sensitivity = 30.0f;
     
     private Transform m_Player;
+    private Transform m_CameraMount;
+    private Transform m_PlayerOrientation;
 
     void Start()
     {
         Cursor.lockState    = CursorLockMode.Locked;
-        m_Player = transform.parent.transform;
+        m_Player = GameObject.Find("Player").transform;
+        m_CameraMount = m_Player.Find("CameraMount");
+        m_PlayerOrientation = m_Player.Find("Orientation");
+        transform.position = m_CameraMount.position;
     }
 
     void LateUpdate()
     {
         Vector2 mouseDelta = Mouse.current.delta.ReadValue();
-        m_Player?.Rotate(Vector3.up, mouseDelta.x * Time.deltaTime * m_Sensitivity);
-
+        transform.Rotate(Vector3.up, mouseDelta.x * Time.deltaTime * m_Sensitivity);
+        m_PlayerOrientation.localEulerAngles = new Vector3(0.0f, transform.localEulerAngles.y, 0.0f);
         m_Pitch -= mouseDelta.y * Time.deltaTime * m_Sensitivity;
         m_Pitch = Mathf.Clamp(m_Pitch, -80.0f, 80.0f);
         transform.localEulerAngles = new Vector3(m_Pitch, transform.localEulerAngles.y, 0.0f);
+        transform.position = m_CameraMount.position;
     }
 }
